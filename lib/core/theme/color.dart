a75bed2a1cb114c5def8ca371b49b9d2b77f3c32
Ahmed Elgammal <elgammal2024@gmail.com>
2024-09-15 16:59:10 +0200

    create architecture and create class color

---
+++ lib/core/theme/color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorsManager {
+  static const Color blue = Color(0xFF5D9CEC);
+  static const Color red = Color(0xFFEC4B4B);
+  static const Color scaffoldLight = Color(0xFFDFECDB);
+  static const Color scaffoldDark = Color(0xFF060E1E);
+}
